1283e4342e0a73167b6183444cae9b73066abcbd
Hakeem <hakeem.trotmanp1@gmail.com>
2022-01-21 16:05:50 -0500

    Created pages Donate, home and about

---
+++ src/pages/donation/donation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const donation = () => {
+  return (
+    <div>
+      <div id="body">
+        <div id="donation_box">donation_box</div>
+      </div>
+    </div>
+  );
+};
+
+export default donation;
