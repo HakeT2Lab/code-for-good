d48311709b827f55940daee600edea0682d16d3d
Sean <sean@Seans-MacBook-Air.local>
2022-01-24 16:43:32 -0500

    Fixed???

---
+++ src/pages/about/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div id="body">
+        <div id="text_and_stat">About</div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
