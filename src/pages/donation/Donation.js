3873f3f540a901710b1feeb5469121b9dd70fbc6
Hakeem <hakeem.trotmanp1@gmail.com>
2022-01-25 15:09:29 -0500

    Finished typing HtML for donation box and added packground image to home.js using url given

---
+++ src/pages/donation/Donation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Donation = () => {
+  return (
+    <div>
+      <div id="body">
+        <div id="donation_box">Donation</div>
+      </div>
+    </div>
+  );
+};
+
+export default Donation;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import "./Home.css";
+
+import Donation_box from "../../components/Donation_box";
 
 const Home = () => {
+
   return (
     <div>
       <div id="body">
-        <div id="donation_box">Home</div>
+        <div className="text">
+          <Typography variant="caption">
+            People like us the world a safer place, let's make someone's life
+            with helping hands for the homeless
+          </Typography>
+          <Donation_box />
+
+        </div>
       </div>
     </div>
   );
